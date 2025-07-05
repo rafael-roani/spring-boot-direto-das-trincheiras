e1cc662bd8fa7a543b4f0b577032336769371056
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-04 22:23:22 -0300

    initial commit

---
+++ anime-service/src/test/java/dev/rafa/animeservice/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafa.animeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
