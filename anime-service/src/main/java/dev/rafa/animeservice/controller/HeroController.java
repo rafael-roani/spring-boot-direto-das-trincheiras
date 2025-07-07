18ccfbb3e31084ce2b40a3d09f5021a40c0d1964
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-06 22:35:34 -0300

    video11: request param

---
+++ anime-service/src/main/java/dev/rafa/animeservice/controller/HeroController.java
@@ -0,0 +1,31 @@
+package dev.rafa.animeservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/heroes")
+public class HeroController {
+
+    private static final List<String> HEROES = List.of("Batman", "Superman", "Spiderman");
+
+    @GetMapping
+    public List<String> listAllHeroes() {
+        return HEROES;
+    }
+
+    @GetMapping("filter")
+    public List<String> listAllHeroesParam(@RequestParam(defaultValue = "") String name) {
+        return HEROES.stream().filter(h -> h.equalsIgnoreCase(name)).toList();
+    }
+
+    @GetMapping("filterList")
+    public List<String> listAllHeroesParamList(@RequestParam List<String> names) {
+        return HEROES.stream().filter(names::contains).toList();
+    }
+
+}
