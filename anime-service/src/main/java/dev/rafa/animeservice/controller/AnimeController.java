c909f7170dfb3218b159edea18a64b4e2966e87e
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-06 22:21:05 -0300

    video10: enabled virtual threads

--- anime-service/src/main/java/dev/rafa/animeservice/controller/AnimeController.java
+++ anime-service/src/main/java/dev/rafa/animeservice/controller/AnimeController.java
@@ -1,5 +1,6 @@
 package dev.rafa.animeservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("v1/animes")
 public class AnimeController {
