e40d6b4529a1149d3b32cb6dce3ba86273623068
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-07 22:22:52 -0300

    video13: execicio RequestParam e PathVariable

--- anime-service/src/main/java/dev/rafa/animeservice/controller/AnimeController.java
+++ anime-service/src/main/java/dev/rafa/animeservice/controller/AnimeController.java
@@ -1,9 +1,12 @@
 package dev.rafa.animeservice.controller;
 
+import dev.rafa.animeservice.domain.Anime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,8 +17,29 @@ import java.util.List;
 public class AnimeController {
 
     @GetMapping
-    public ResponseEntity<List<String>> listAll() {
-        return ResponseEntity.ok(List.of("One Piece", "Naruto"));
+    public ResponseEntity<List<Anime>> listAll(@RequestParam(required = false) String name) {
+        List<Anime> animes;
+
+        if (name == null) {
+            animes = Anime.getAnimes();
+        } else {
+            animes = Anime.getAnimes()
+                    .stream()
+                    .filter(a -> a.getName().equalsIgnoreCase(name))
+                    .toList();
+        }
+
+        return ResponseEntity.ok(animes);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(Anime.getAnimes()
+                .stream()
+                .filter(a -> a.getId().equals(id))
+                .findFirst()
+                .orElse(null)
+        );
     }
 
 }
