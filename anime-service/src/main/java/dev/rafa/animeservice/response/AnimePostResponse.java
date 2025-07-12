99b87a014d5827f7064457aca1bf3c57bd4f7ec8
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-11 22:22:32 -0300

    video20: Exercicio MapStruct

---
+++ anime-service/src/main/java/dev/rafa/animeservice/response/AnimePostResponse.java
@@ -0,0 +1,16 @@
+package dev.rafa.animeservice.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class AnimePostResponse {
+
+    private Long id;
+
+    private String name;
+
+}
