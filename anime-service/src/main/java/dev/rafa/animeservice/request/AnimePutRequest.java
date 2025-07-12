65defdc107c7af25da54b29d0fc8113813335d8e
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-11 23:46:35 -0300

    video24: PutMapping

---
+++ anime-service/src/main/java/dev/rafa/animeservice/request/AnimePutRequest.java
@@ -0,0 +1,16 @@
+package dev.rafa.animeservice.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class AnimePutRequest {
+
+    private Long id;
+
+    private String name;
+
+}
