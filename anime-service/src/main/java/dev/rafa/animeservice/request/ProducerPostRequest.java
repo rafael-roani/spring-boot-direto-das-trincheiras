defa729d21a5b43c2052ca51737cbfc6cfb56018
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-11 22:45:21 -0300

    video21: Atualizando Producer

--- anime-service/src/main/java/dev/rafa/animeservice/request/ProducerPostRequest.java
+++ anime-service/src/main/java/dev/rafa/animeservice/request/ProducerPostRequest.java
@@ -2,9 +2,11 @@ package dev.rafa.animeservice.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class ProducerPostRequest {
 
     private String name;
