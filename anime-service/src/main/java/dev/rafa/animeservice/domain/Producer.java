bd1420376c37e8f464b8e731046b2cb227d76a81
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-12 00:13:28 -0300

    video26: Padrão MVC Repository

--- anime-service/src/main/java/dev/rafa/animeservice/domain/Producer.java
+++ anime-service/src/main/java/dev/rafa/animeservice/domain/Producer.java
@@ -3,11 +3,8 @@ package dev.rafa.animeservice.domain;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Builder
@@ -19,32 +16,5 @@ public class Producer {
     private String name;
 
     private LocalDateTime createdAt;
-
-    @Getter
-    private static List<Producer> producers = new ArrayList<>();
-
-    static {
-        producers.add(
-                Producer.builder()
-                        .id(1L)
-                        .name("Mappa")
-                        .createdAt(LocalDateTime.now())
-                        .build()
-        );
-        producers.add(
-                Producer.builder()
-                        .id(2L)
-                        .name("Kyoto Animation")
-                        .createdAt(LocalDateTime.now())
-                        .build()
-        );
-        producers.add(
-                Producer.builder()
-                        .id(3L)
-                        .name("Madhouse")
-                        .createdAt(LocalDateTime.now())
-                        .build()
-        );
-    }
 
 }
