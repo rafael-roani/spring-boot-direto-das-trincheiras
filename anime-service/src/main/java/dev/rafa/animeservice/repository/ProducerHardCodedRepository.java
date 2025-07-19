11e6630bd42a174d7c2d5dbc30c330950ace64a3
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-18 22:33:12 -0300

    video34: Testes Unitários - Repositório pt 03

--- anime-service/src/main/java/dev/rafa/animeservice/repository/ProducerHardCodedRepository.java
+++ anime-service/src/main/java/dev/rafa/animeservice/repository/ProducerHardCodedRepository.java
@@ -7,8 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
