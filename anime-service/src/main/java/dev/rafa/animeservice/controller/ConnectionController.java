73caed60a87aa98e8bedc265309056ff8385b9e3
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-26 20:07:01 -0300

    video51: Spring Profiles pt 03

--- anime-service/src/main/java/dev/rafa/animeservice/controller/ConnectionController.java
+++ anime-service/src/main/java/dev/rafa/animeservice/controller/ConnectionController.java
@@ -1,6 +1,6 @@
 package dev.rafa.animeservice.controller;
 
-import external.dependency.Connection;
+import dev.rafa.animeservice.config.Connection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
