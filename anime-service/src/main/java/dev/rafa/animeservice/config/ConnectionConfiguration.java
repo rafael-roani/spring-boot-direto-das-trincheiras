a8cca209a9a062e393c31e22191f3228cec5f6cb
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-25 22:47:17 -0300

    video49: Spring Profiles pt 01

--- anime-service/src/main/java/dev/rafa/animeservice/config/ConnectionConfiguration.java
+++ anime-service/src/main/java/dev/rafa/animeservice/config/ConnectionConfiguration.java
@@ -1,17 +1,26 @@
 package dev.rafa.animeservice.config;
 
 import external.dependency.Connection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ConnectionConfiguration {
 
+    @Value("${database.url}")
+    private String url;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
     @Bean
     // @Primary
     public Connection connectionMySql() {
-        return new Connection("localhost", "devdojoMySql", "goku");
+        return new Connection(url, username, password);
     }
 
     @Bean
---
+++ anime-service/src/main/java/dev/rafa/animeservice/controller/ConnectionController.java
@@ -0,0 +1,24 @@
+package dev.rafa.animeservice.controller;
+
+import external.dependency.Connection;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/connections")
+public class ConnectionController {
+
+    private final Connection connectionMySql;
+
+    @GetMapping
+    public ResponseEntity<Connection> getConnection() {
+        return ResponseEntity.ok(connectionMySql);
+    }
+
+}
