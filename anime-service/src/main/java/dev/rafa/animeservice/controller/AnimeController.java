1ebfc78eb262d0f0f79e6a745b8b2776d53e5256
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-07-26 23:28:52 -0300

    video53: @ConfigurationProperties

--- anime-service/src/main/java/dev/rafa/animeservice/controller/AnimeController.java
+++ anime-service/src/main/java/dev/rafa/animeservice/controller/AnimeController.java
@@ -11,15 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -80,7 +72,7 @@ public class AnimeController {
     public ResponseEntity<Void> update(@RequestBody AnimePutRequest request) {
         log.debug("Updating anime: {}", request);
 
-        Anime animeToUpdated =  mapper.toAnime(request);
+        Anime animeToUpdated = mapper.toAnime(request);
 
         service.update(animeToUpdated);
 
