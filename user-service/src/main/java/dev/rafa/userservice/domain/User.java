c79ccc4b653551eba00e768960d2cac966d9e94a
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-08-24 13:20:05 -0300

    video76: Criando UserRepository

--- user-service/src/main/java/dev/rafa/userservice/domain/User.java
+++ user-service/src/main/java/dev/rafa/userservice/domain/User.java
@@ -1,21 +1,28 @@
 package dev.rafa.userservice.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
 @Setter
+@Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String firstName;
 
+    @Column(nullable = false)
     private String lastName;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
 }
