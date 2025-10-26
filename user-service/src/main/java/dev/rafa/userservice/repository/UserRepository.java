deaddbd3ddca260e5a48dbf8577297d55bbf8927
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-10-26 15:26:06 -0300

    video79: Spring Query Builder a validação do email

--- user-service/src/main/java/dev/rafa/userservice/repository/UserRepository.java
+++ user-service/src/main/java/dev/rafa/userservice/repository/UserRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findByFirstNameIgnoreCase(String firstName);
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    Optional<User> findByEmailIgnoreCaseAndIdNot(String email, Long id);
 
 }
--- user-service/src/main/java/dev/rafa/userservice/service/UserService.java
+++ user-service/src/main/java/dev/rafa/userservice/service/UserService.java
@@ -4,7 +4,9 @@ import dev.rafa.commonscore.exception.NotFoundException;
 import dev.rafa.userservice.domain.User;
 import dev.rafa.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,6 +27,7 @@ public class UserService {
 
     public void update(User userToUpdate) {
         assertUserExists(userToUpdate.getId());
+        assertEmailDoesNotExist(userToUpdate.getEmail(), userToUpdate.getId());
         repository.save(userToUpdate);
     }
 
@@ -38,7 +41,22 @@ public class UserService {
     }
 
     public User save(User user) {
+        assertEmailDoesNotExist(user.getEmail());
         return repository.save(user);
+    }
+
+    public void assertEmailDoesNotExist(String email) {
+        repository.findByEmailIgnoreCase(email)
+                .ifPresent(this::throwEmailExistsException);
+    }
+
+    public void assertEmailDoesNotExist(String email, Long id) {
+        repository.findByEmailIgnoreCaseAndIdNot(email, id)
+                .ifPresent(this::throwEmailExistsException);
+    }
+
+    private void throwEmailExistsException(User user) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "E-mail %s already exists".formatted(user.getEmail()));
     }
 
 }
