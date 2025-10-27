170731a0da319686c4e8779758b75585a0807362
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-10-26 23:46:19 -0300

    video81: Exercício Spring Data Anime Service pt 01 - Limpando a casa

--- anime-service/src/main/java/dev/rafa/animeservice/AnimeServiceApplication.java
+++ anime-service/src/main/java/dev/rafa/animeservice/AnimeServiceApplication.java
@@ -1,14 +1,11 @@
 package dev.rafa.animeservice;
 
-import dev.rafa.animeservice.config.ConnectionConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"dev.rafa"})
-@EnableConfigurationProperties(ConnectionConfigurationProperties.class)
 public class AnimeServiceApplication {
 
     public static void main(String[] args) {
