b372eb4ab02ea43ecd7bedca79441f975914aa50
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-10-27 19:49:45 -0300

    video82: Exercício Spring Data Anime Service pt 02 - Transformando Anime em entidade

--- anime-service/src/main/java/dev/rafa/animeservice/domain/Producer.java
+++ anime-service/src/main/java/dev/rafa/animeservice/domain/Producer.java
@@ -1,16 +1,24 @@
 package dev.rafa.animeservice.domain;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Producer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
