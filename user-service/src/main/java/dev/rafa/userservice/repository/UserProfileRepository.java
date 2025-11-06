9b67da60ee91d8963619127238d78d02a9474426
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-11-06 20:57:47 -0300

    video97: Queries com JPQL

--- user-service/src/main/java/dev/rafa/userservice/repository/UserProfileRepository.java
+++ user-service/src/main/java/dev/rafa/userservice/repository/UserProfileRepository.java
@@ -2,10 +2,17 @@ package dev.rafa.userservice.repository;
 
 import dev.rafa.userservice.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
 
+@Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+    @Query("SELECT up FROM UserProfile up " +
+            " JOIN FETCH up.user u " +
+            " JOIN FETCH up.profile p ")
+    List<UserProfile> retrieveAll();
 
 }
