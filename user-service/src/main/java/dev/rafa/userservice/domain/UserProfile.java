0377dd40df7dd038a709d2851a583067588fa675
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2025-11-06 19:38:03 -0300

    video93: Spring Data Jpa Many to Many 01

---
+++ user-service/src/main/java/dev/rafa/userservice/domain/UserProfile.java
@@ -0,0 +1,27 @@
+package dev.rafa.userservice.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@With
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class UserProfile {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    @ManyToOne(optional = false)
+    private Profile profile;
+
+}
