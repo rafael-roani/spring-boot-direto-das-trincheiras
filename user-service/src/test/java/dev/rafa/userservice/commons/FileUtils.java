ba3207030d23d27e6391970093ef5fb7bbb90512
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2026-02-04 21:27:20 -0300

    video112: RestAssured - pt 01

--- user-service/src/test/java/dev/rafa/userservice/commons/FileUtils.java
+++ user-service/src/test/java/dev/rafa/userservice/commons/FileUtils.java
@@ -1,11 +1,11 @@
 package dev.rafa.userservice.commons;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 
 @Component
@@ -14,7 +14,8 @@ public class FileUtils {
     @Autowired
     private ResourceLoader resourceLoader;
 
-    public String readResourceFile(String fileName) throws IOException {
+    @SneakyThrows
+    public String readResourceFile(String fileName) {
         File file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
         return new String(Files.readAllBytes(file.toPath()));
     }
