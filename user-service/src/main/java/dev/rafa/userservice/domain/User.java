8e3615dc097a4b07549dd9cc2ff8efcfdc21cf4f
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2026-03-13 20:52:39 -0300

    video134: Spring Security 07 - Autenticação e autorização com banco de dados 01

--- user-service/src/main/java/dev/rafa/userservice/domain/User.java
+++ user-service/src/main/java/dev/rafa/userservice/domain/User.java
@@ -2,6 +2,12 @@ package dev.rafa.userservice.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @With
 @Getter
@@ -11,7 +17,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class User {
+public class User implements UserDetails {
 
     @Id
     @EqualsAndHashCode.Include
@@ -26,5 +32,28 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String roles; // ADMIN,USER...
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(roles.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
 
 }
---
+++ user-service/src/main/java/dev/rafa/userservice/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package dev.rafa.userservice.service;
+
+import dev.rafa.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmailIgnoreCase(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found for email: " + email));
+    }
+
+}
