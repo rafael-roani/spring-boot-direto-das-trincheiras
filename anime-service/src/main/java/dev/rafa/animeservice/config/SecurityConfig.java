9784f3d1bf5f3e36fb4db283fcb098b05b0a6663
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2026-04-05 21:00:30 -0300

    video150: Obervabilidade Actuator e Prometheus 01

--- anime-service/src/main/java/dev/rafa/animeservice/config/SecurityConfig.java
+++ anime-service/src/main/java/dev/rafa/animeservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package dev.rafa.animeservice.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -43,6 +44,7 @@ public class SecurityConfig {
                                 .requestMatchers(WHITE_LIST).permitAll()
                                 .requestMatchers("v1/animes/**").hasRole("USER")
                                 .requestMatchers("v1/producers/**").hasRole("USER")
+                                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
