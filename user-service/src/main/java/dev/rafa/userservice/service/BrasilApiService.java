dd7964dbfeb4d996f29ecba7a4efd4c9c2370d00
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2026-04-29 11:37:01 -0300

    video158: RestClient 02

--- user-service/src/main/java/dev/rafa/userservice/service/BrasilApiService.java
+++ user-service/src/main/java/dev/rafa/userservice/service/BrasilApiService.java
@@ -1,8 +1,12 @@
 package dev.rafa.userservice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.rafa.commonscore.exception.NotFoundException;
 import dev.rafa.userservice.config.BrasilApiConfigurationProperties;
+import dev.rafa.userservice.response.CepErrorResponse;
 import dev.rafa.userservice.response.CepGetResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -14,11 +18,18 @@ public class BrasilApiService {
 
     private final BrasilApiConfigurationProperties brasilApiConfigurationProperties;
 
+    private final ObjectMapper mapper;
+
     public CepGetResponse findCep(String cep) {
         return brasilApiClient.build()
                 .get()
                 .uri(brasilApiConfigurationProperties.cepUri(), cep)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, ((request, response) -> {
+                    String body = new String(response.getBody().readAllBytes());
+                    CepErrorResponse cepErrorResponse = mapper.readValue(body, CepErrorResponse.class);
+                    throw new NotFoundException(cepErrorResponse.toString());
+                }))
                 .body(CepGetResponse.class);
     }
 
