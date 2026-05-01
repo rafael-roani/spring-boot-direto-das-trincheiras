eab0a4eb4aa2adec195765308647a52e518715f6
TaldoRafa <rafaelgoncalvesroani@gmail.com>
2026-05-01 16:20:26 -0300

    video165: Migração de banco de dados com Flyway

---
+++ anime-service/src/main/resources/db/migration/V2__initialize_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO anime_service.anime (id, name)
+VALUES (1, 'Desencanto');
+INSERT INTO anime_service.anime (id, name)
+VALUES (2, 'Goku');
+INSERT INTO anime_service.anime (id, name)
+VALUES (3, 'DBZ');
+INSERT INTO anime_service.anime (id, name)
+VALUES (4, 'Naruto');
+INSERT INTO anime_service.anime (id, name)
+VALUES (5, 'Full Metal');
+INSERT INTO anime_service.anime (id, name)
+VALUES (6, 'Pokemon');
+INSERT INTO anime_service.anime (id, name)
+VALUES (7, 'Steins Gate');
